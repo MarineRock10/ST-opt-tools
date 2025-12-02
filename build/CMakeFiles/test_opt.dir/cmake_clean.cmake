ee9d26f4aeb72636d615da19b193fac8a08f781e
tansenming <senmingtan@wlrobo.com>
2025-12-02 10:01:58 +0800

    new doc.opt.png

---
+++ build/CMakeFiles/test_opt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_opt.dir/src/test_opt.cpp.o"
+  "CMakeFiles/test_opt.dir/src/test_opt.cpp.o.d"
+  "test_opt"
+  "test_opt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
